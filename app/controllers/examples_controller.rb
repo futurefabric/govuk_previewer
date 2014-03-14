e12dbb9f01b20c90a3c2933b67e66e390dd5c5ca
Guy Moorhouse <gmoorhouse@gmail.com>
2014-03-14 14:32:37 +0000

    Set up http auth on root In production only. Not totally secure, but enough for this

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,7 +1,20 @@
 class ExamplesController < ApplicationController
-	def index
-		@examples = get_examples
+
+before_filter :authenticate
+
+def index
+	@examples = get_examples
+end
+
+protected
+
+def authenticate
+	if Rails.env.production?
+	  authenticate_or_request_with_http_basic do |username, password|
+	    username == ENV['USERNAME'] && password == ENV['PASSWORD']
+	  end
 	end
+end
 
 private
 	def get_examples
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 GovukPreviewer::Application.routes.draw do
-  get "examples", to: "examples#index"
+  root "examples#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
